972f40b3664b471388542e19eea2d8066e24e04e
Fabio Carucci <garu2000@live.it>
2024-04-11 19:15:29 +0200

    Connessione al database MongoDB

--- m6w1d1 - backend/server.js
+++ m6w1d1 - backend/server.js
@@ -1,18 +1,37 @@
-const express = require('express'); // Importa il framework Express per gestire le richieste HTTP
-const mongoose = require('mongoose'); // Importa il modulo mongoose per interagire con il database MongoDB
-const cors = require('cors'); // Importa il modulo cors per interagire con il frontend
-
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const authorsRoutes = require('./routes/authorsRoutes');
 
-const app = express();
 const PORT = process.env.PORT || 5002;
+const db = process.env.DB_URL;
+
+const app = express();
+
+// Middleware per consentire le richieste CORS dal frontend
+app.use(cors());
+
+// Middleware per analizzare i body delle richieste in formato JSON
+app.use(express.json());
+
+// Connessione al database MongoDB utilizzando Mongoose
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db);
+        console.log('Connessione al database MongoDB riuscita');
+        // Avvia il server solo dopo che la connessione al database è stata stabilita con successo
+        app.listen(PORT, () => {
+            console.log(`Il server è in ascolto sulla porta ${PORT}`);
+        });
+    } catch (error) {
+        console.error('Errore durante la connessione al database MongoDB:', error);
+    }
+};
+
+// Invocazione della funzione di connessione al database
+connectDB();
 
 // Utilizza la route degli autori nel server
 app.use('/api', authorsRoutes);
-
-// Avvia il server
-app.listen(PORT, () => {
-    console.log(`Server in ascolto sulla porta ${PORT}`);
-});
